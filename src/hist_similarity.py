03124727c300acebd13249039a375a9368841f82
xingmin.li <xingmin.li@enflame-tech.com>
2021-12-30 19:33:52 +0800

    alg works

--- src/hist_similarity.py
+++ src/hist_similarity.py
@@ -1,4 +1,4 @@
-import cv
+import cv2 as cv
 
 
 def compare_img_hist(img1, img2):
@@ -16,10 +16,18 @@ def compare_img_hist(img1, img2):
     # Get the histogram data of image 1, then using normalize the picture for better compare
     img1_hist = cv.calcHist([img1], [1], None, [256], [0, 256])
     img1_hist = cv.normalize(img1_hist, img1_hist, 0, 1, cv.NORM_MINMAX, -1)
-    ​
+
     img2_hist = cv.calcHist([img2], [1], None, [256], [0, 256])
     img2_hist = cv.normalize(img2_hist, img2_hist, 0, 1, cv.NORM_MINMAX, -1)
-    ​
+
     similarity = cv.compareHist(img1_hist, img2_hist, 0)
-    ​
+
     return similarity
+
+if __name__ == "__main__":
+    refer_dir = "../img/refer.png"
+    test_dir = "../img/late.png"
+    refer = cv.imread(refer_dir)
+    test = cv.imread(test_dir)
+    similarity = compare_img_hist(refer, test)
+    print("The Similarity of test to reference = {}".format(similarity))
--- src/p_hash_similarity.py
+++ src/p_hash_similarity.py
@@ -1,4 +1,63 @@
-import cv
+import cv2 as cv
+import numpy as np
+
+def get_img_p_hash(img):
+    """
+        Get the pHash value of the image, pHash : Perceptual hash algorithm(感知哈希算法)                                  ​
+    :param img: img in MAT format(img = cv2.imread(image))
+    :return: pHash value
+    """
+    hash_len = 32
+
+    # GET Gray image
+    gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+    # Resize image, use the different way to get the best result
+    resize_gray_img = cv.resize(
+        gray_img, (hash_len, hash_len), cv.INTER_AREA)
+    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_LANCZOS4)
+    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_LINEAR)
+    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_NEAREST)
+    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_CUBIC)
+
+    # Change the int of image to float, for better DCT
+    h, w = resize_gray_img.shape[:2]
+    vis0 = np.zeros((h, w), np.float32)
+    vis0[:h, :w] = resize_gray_img
+
+    # DCT: Discrete cosine transform(离散余弦变换)
+    vis1 = cv.dct(cv.dct(vis0))
+    vis1.resize(hash_len, hash_len)
+    img_list = vis1.flatten()
+
+    # Calculate the avg value
+    avg = sum(img_list) * 1. / len(img_list)
+    avg_list = []
+    for i in img_list:
+        if i < avg:
+            tmp = '0'
+        else:
+            tmp = '1'
+            avg_list.append(tmp)
+
+            # Calculate the hash value
+            p_hash_str = ''
+            for x in range(0, hash_len * hash_len, 4):
+                p_hash_str += '%x' % int(
+                    ''.join(avg_list[x:x + 4]), 2)
+                return p_hash_str
+
+
+def ham_dist(x, y):
+    """
+        Get the hamming distance of two values.
+    hamming distance(汉明距)
+    :param x:
+    :param y:
+    :return: the hamming distance
+    """
+    assert len(x) == len(y)
+    return sum([ch1 != ch2 for ch1, ch2 in zip(x, y)])
 
 
 def compare_img_p_hash(img1, img2):
@@ -16,68 +75,14 @@ def compare_img_p_hash(img1, img2):
     """
     hash_img1 = get_img_p_hash(img1)
     hash_img2 = get_img_p_hash(img2)
-    ​
+
     return ham_dist(hash_img1, hash_img2)
-    ​
-    ​
 
 
-def get_img_p_hash(img):
-    """
-        Get the pHash value of the image, pHash : Perceptual hash algorithm(感知哈希算法)                                  ​
-    :param img: img in MAT format(img = cv2.imread(image))
-    :return: pHash value
-    """
-    hash_len = 32
-    ​
-    # GET Gray image
-    gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    ​
-    # Resize image, use the different way to get the best result
-    resize_gray_img = cv.resize(
-        gray_img, (hash_len, hash_len), cv.INTER_AREA)
-    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_LANCZOS4)
-    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_LINEAR)
-    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_NEAREST)
-    # resize_gray_img = cv.resize(gray_img, (hash_len, hash_len), cv.INTER_CUBIC)
-    ​
-    # Change the int of image to float, for better DCT
-    h, w = resize_gray_img.shape[:2]
-    vis0 = np.zeros((h, w), np.float32)
-    vis0[:h, :w] = resize_gray_img
-    ​
-    # DCT: Discrete cosine transform(离散余弦变换)
-    vis1 = cv.dct(cv.dct(vis0))
-    vis1.resize(hash_len, hash_len)
-    img_list = vis1.flatten()
-    ​
-    # Calculate the avg value
-    avg = sum(img_list) * 1. / len(img_list)
-    avg_list = []
-    for i in img_list:
-        if i < avg:
-            tmp = '0'
-            else:
-                tmp = '1'
-                avg_list.append(tmp)
-                ​
-                # Calculate the hash value
-                p_hash_str = ''
-                for x in range(0, hash_len * hash_len, 4):
-                    p_hash_str += '%x' % int(
-                        ''.join(avg_list[x:x + 4]), 2)
-                    return p_hash_str
-                    ​
-                    ​
-
-
-def ham_dist(x, y):
-    """
-        Get the hamming distance of two values.
-    hamming distance(汉明距)
-    :param x:
-    :param y:
-    :return: the hamming distance
-    """
-    assert len(x) == len(y)
-    return sum([ch1 != ch2 for ch1, ch2 in zip(x, y)])
+if __name__ == "__main__":
+    refer_dir = "../img/refer.png"
+    test_dir = "../img/early.png"
+    refer = cv.imread(refer_dir)
+    test = cv.imread(test_dir)
+    similarity = compare_img_p_hash(refer, test)
+    print("The Similarity of test to reference = {}".format(similarity))
